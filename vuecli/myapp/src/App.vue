380106b3d6695b7d6fd8f9ca3cf8e982fcc56a08
fengchuanyu <fengchuanyu@163.com>
2019-08-01 16:57:00 +0800

    admin

---
+++ vuecli/myapp/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Navgation></Navgation>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Navgation from "@/components/Navagation.vue"
+export default {
+  components:{
+    Navgation
+  }
+}
+</script>
+<style>
+  *{
+    margin : 0;
+    padding: 0;
+  }
+  a{
+    text-decoration: none;
+  }
+  li{
+    list-style: none;
+  }
+</style>
