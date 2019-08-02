483b7cc3b341f25f46f5a699594575f8e94b8195
fengchuanyu <fengchuanyu@163.com>
2019-08-02 14:35:34 +0800

    admin

--- vuecli/myapp/src/views/movie/Movie.vue
+++ vuecli/myapp/src/views/movie/Movie.vue
@@ -5,13 +5,29 @@
 </template>
 <script>
 export default {
+    data() {
+        return {
+            dataList:[]
+        }
+    },
     created() {
+        this.getData();
         let obj = {
             title:"电影",
             navName:"movie"
         }
         this.$emit("changeActive",obj)
     },
+    methods: {
+        getData(){
+            let birdapi = 'https://bird.ioliu.cn/v2?url='
+            let urlapi = `https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items?start=0&count=10`
+            axios.get(birdapi+urlapi)
+            .then((res)=>{
+                console.log(res.data.subject_collection_items);
+            })
+        }
+    },  
 }
 </script>
 <style scoped>
